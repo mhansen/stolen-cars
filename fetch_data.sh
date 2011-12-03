145463a42f226e3a48e716834b9bcb55ea10a7af
Mark Hansen <mark@markhansen.co.nz>
2011-12-03 18:03:33 +1300

    Create script to download the database from the cops.

---
+++ fetch_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -s -d form_id=stolen_vehicles_download_form \
+        -d op=Download \
+        -d 'nz%5Ball%5D=all'\
+        http://www.police.govt.nz/stolen/vehicles | \
+funzip | python csv_to_json.py > stolenvehicles.json
